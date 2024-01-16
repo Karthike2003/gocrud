b2b15def49cf0aa7aefadc7230be8ee1b7713754
Karthik <karthiksujith789@gmail.com>
2024-01-16 22:35:06 +0530

    todays work is done

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"encoding/json"
+	 "math/rand"// for id generation
+	 "net/http"
+	 "strconv"
+	 "github.com/gorilla/mux"
+)
+
+
+
+type Movie struct {
+	ID string `json:"id"`
+	Isbn string `json:"isbn"`
+	Title string `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+	Firstname string `json:"firstname"`
+	Lastname string `json:"lastname"`
+}
